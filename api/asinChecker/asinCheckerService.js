361eed9603c4b67eeb29494bba449d4ba987fb50
robertocsp <80.pereira@gmail.com>
2018-11-20 23:58:07 -0500

    alteracoes para servidor https

--- api/asinChecker/asinCheckerService.js
+++ api/asinChecker/asinCheckerService.js
@@ -2,17 +2,28 @@
 
 //AsinChecker.methods(['get', 'post', 'put', 'delete'])
 
+const mwsEndPoint = 'https://mws.amazonservices.com'
+const mktPlaceId = 'ATVPDKIKX0DER'
+
+
+/*Amazon fornece no header a resposta do total de cotas do operador, quantas cotas restantes e data e hora que as cotas irão resetar ex:
+x-mws-quota-max: 3600
+x-mws-quota-remaining: 10
+x-mws-quota-resetsOn: Wed, 06 Mar 2013 19:07:58 GMT
+*/
+
 function checkAsinsInInventory(req, res){
-  console.log(req.body)
 
   var user_plataforma = req.body.user_plataforma
   var nome_loja = req.body.nome_loja
   var token_loja = req.body.token_loja
-
+  console.log(req.body.user_plataforma)
   for (i in req.body.produtos){
     console.log(`ASIN produto dentro do for: ${req.body.produtos[i].asin}`)
     console.log(`ASIN produto dentro do for: ${req.body.produtos[i].quantidade}`)
     //Pegar os valores do ASIN e primeiramente verificar se os produtos estão no inventário do cliente
+
+
   }
 
 
--- config/server.js
+++ config/server.js
@@ -1,20 +1,40 @@
 const port = 3030
-const devId = process.env.DEV_ID
-const azMktPlace = process.env.AZ_MKT_PLACE
+//const https = require('https')
+//const devId = process.env.DEV_ID
+//const azMktPlace = process.env.AZ_MKT_PLACE
 
+const fs = require('fs');
+const https = require('https');
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+
+const production = true;
+
+//Certificado https
+// Certificate
+if (production){
+  const options = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/app.voiservices.com/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/app.voiservices.com/privkey.pem')
+  }
+}
+
 
 
 //Este middleware diz para o server, se os dados foram passados por um formulario.
 server.use(bodyParser.urlencoded({ extended: true }))
 
 //Verificar se dentro da requisicao o corpo do conteudo é um JSON
-//server.use(bodyParser.json())
+server.use(bodyParser.json())
+
+
+
 
 server.listen(port, function(){
   console.log(`backend is running on port ${port}.`)
 })
-
+if (production){
+  https.createServer(options, server).listen(8443);
+}
 module.exports = server
