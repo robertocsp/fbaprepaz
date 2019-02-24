fb797b9b8c147841048cdc79758419b7a32f5bfa
robertocsp <80.pereira@gmail.com>
2019-02-24 18:02:10 -0500

    depurando o problema do no inventario

--- api/inventoryBuilding/inventoryBuildingService.js
+++ api/inventoryBuilding/inventoryBuildingService.js
@@ -62,7 +62,7 @@ function insertManifest(req, res){
       //Tenho que verificar se o produto já existe para incrementar a quantidade, e se não existir, gravar como unmanifested
       //InventoryBuilding.create(objetoJson)
 
-      var query = {'asin': objetoJson.asin }
+      var query = {'asin': objetoJson.asin, inventarioNumero: objetoJson.inventarionumero, inventarioterminal: objetoJson.inventarioterminal }
       InventoryBuilding.findOneAndUpdate(query, { $inc: { qty: 1 } }, { new: true }, function(err, doc){
         console.log(`doc: ${doc}`)
         console.log(`inventarioNumero: ${inventarioNumero}`)
