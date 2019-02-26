e0622a06f62b35d7822479179684b21b9d30fcf2
robertocsp <80.pereira@gmail.com>
2019-02-26 01:47:54 -0500

    depurando problema server

--- config/routes.js
+++ config/routes.js
@@ -17,6 +17,7 @@ module.exports = function(server){
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type');
+      console.log('colocou o cabecalho no response')
       next();
   })
 
